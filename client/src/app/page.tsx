7973b2109b9f076d2ea45b657520f79711022d19
Heri Roque <heriroque.22@gmail.com>
2024-11-29 21:51:27 -0500

    refactor vehicle management form to support edit mode and improve state handling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,13 +10,14 @@ enum EstadosEnum {
 }
 
 type Inputs = {
+  id?: number;
   marca: string;
   modelo: string;
-  estado: string;
+  estado: EstadosEnum;
 };
 
 interface Vehiculo {
-  id: number;
+  id?: number;
   marca: string;
   modelo: string;
   estado: EstadosEnum;
@@ -29,39 +30,96 @@ export default function Home() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    if (modo === "editar") {
+      updateVehiculo(data.id!, {
+        marca: data.marca,
+        modelo: data.modelo,
+        estado: data.estado as EstadosEnum,
+      });
+      return;
+    }
+
+    addVehiculo({
+      marca: data.marca,
+      modelo: data.modelo,
+      estado: data.estado as EstadosEnum,
+    });
+  };
 
   const [vehiculos, setVehiculos] = useState<Vehiculo[]>([]);
+  const [modo, setModo] = useState("crear");
 
   useEffect(() => {
     async function getVehiculos() {
       const response = await fetch(url);
       const data = await response.json();
       setVehiculos(data);
+      console.log(vehiculos);
     }
 
     getVehiculos();
-
-    console.log(vehiculos);
   }, []);
 
-  const deleteVehiculo = async (id: number) => {
-    await fetch(`${url}${id}`, {
-      method: "DELETE",
-    });
-  };
-
-  const updateVehiculo = async (id: number, vehiculo: Vehiculo) => {
-    await fetch(`${url}${id}`, {
-      method: "PUT",
+  const addVehiculo = async (vehiculo: Vehiculo) => {
+    const response = await fetch(url, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(vehiculo),
     });
 
+    const { id } = await response.json();
+
+    vehiculo = { ...vehiculo, id };
+
+    setVehiculos([...vehiculos, vehiculo]);
+  };
+
+  const deleteVehiculo = async (id: number) => {
+    await fetch(`${url}/${id}`, {
+      method: "DELETE",
+    });
+
+    setVehiculos(vehiculos.filter((v) => v.id !== id));
+  };
+
+  const modoEditar = (id: number, vehiculo: Vehiculo) => {
+    setModo("editar");
+    setValue("id", id);
+    setValue("marca", vehiculo.marca);
+    setValue("modelo", vehiculo.modelo);
+    setValue("estado", vehiculo.estado);
+  };
+
+  const updateVehiculo = async (id: number, vehiculo: Vehiculo) => {
+    await fetch(`${url}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(vehiculo),
+    });
+
+    setValue("id", undefined);
+    setValue("marca", "");
+    setValue("modelo", "");
+    setValue("estado", EstadosEnum.disponible);
+    setModo("crear");
+
+    setVehiculos(
+      vehiculos.map((v) => {
+        if (v.id === id) {
+          vehiculo.id = id;
+          return vehiculo;
+        }
+        return v;
+      })
+    );
     console.log(vehiculo);
   };
 
@@ -69,6 +127,7 @@ export default function Home() {
     <div className="">
       <h1>Gestión de Vehículos</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("id")} hidden />
         <label htmlFor="marca">Marca:</label>
         <input {...register("marca", { required: true })} />
 
@@ -82,7 +141,9 @@ export default function Home() {
           <option value="mantenimiento">Mantenimiento</option>
         </select>
 
-        <button type="submit">Añadir Vehículo</button>
+        <button type="submit">
+          {modo === "crear" ? "Añadir" : "Editar"} Vehículo
+        </button>
       </form>
 
       <table id="vehicle-table">
@@ -101,15 +162,15 @@ export default function Home() {
               <td>{vehiculo.id}</td>
               <td>{vehiculo.marca}</td>
               <td>{vehiculo.modelo}</td>
-              <td>{vehiculo.estado}</td>
+              <td className="uppercase">{vehiculo.estado}</td>
               <td>
                 <button
                   className="mr-2"
-                  onClick={() => deleteVehiculo(vehiculo.id)}
+                  onClick={() => deleteVehiculo(vehiculo.id!)}
                 >
                   Eliminar
                 </button>
-                <button onClick={() => updateVehiculo(vehiculo.id, vehiculo)}>
+                <button onClick={() => modoEditar(vehiculo.id!, vehiculo)}>
                   Actualizar
                 </button>
               </td>
