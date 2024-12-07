875ae106e331d1d2c10d075b30c8827d96282fcf
Heri Roque <heriroque.22@gmail.com>
2024-12-07 13:41:55 -0500

    feat: add CRUD routes for Vehiculo in the router

--- back/src/routes/vehiculo.router.ts
+++ back/src/routes/vehiculo.router.ts
@@ -1,10 +1,13 @@
 // src/routes/vehiculoRoutes.ts
 import { Router } from 'express';
-import { getVehiculos, createVehiculo } from '../controllers/vehiculo.controller';
+import { getVehiculos, createVehiculo, deleteVehiculo, updateVehiculo, getVehiculo } from '../controllers/vehiculo.controller';
 
 const router = Router();
 
 router.get('/vehiculos', getVehiculos);
 router.post('/vehiculos', createVehiculo);
+router.get('/vehiculos/:id', getVehiculo);
+router.put('/vehiculos/:id', updateVehiculo);
+router.delete('/vehiculos/:id', deleteVehiculo);
 
 export default router;
