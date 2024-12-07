6c034abc4ec57a2be7a39bec4065a5733fe01779
Heri Roque <heriroque.22@gmail.com>
2024-12-07 13:41:45 -0500

    feat: add CRUD operations for Vehiculo in the controller

--- back/src/controllers/vehiculo.controller.ts
+++ back/src/controllers/vehiculo.controller.ts
@@ -22,3 +22,34 @@ export const createVehiculo = async (req: Request, res: Response) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+export const getVehiculo = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const vehiculo = await Vehiculo.findById(id);
+    res.json(vehiculo);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const updateVehiculo = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const updatedVehiculo = await Vehiculo.findByIdAndUpdate(id, req.body as typeof Vehiculo, { new: true });
+    res.json(updatedVehiculo);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const deleteVehiculo = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await Vehiculo.findByIdAndDelete(id);
+    res.json({ message: 'Vehiculo deleted' });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
