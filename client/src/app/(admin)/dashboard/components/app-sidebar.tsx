84f66e3323c2507d08107a1a27edbc2d6ea22d95
Heri Roque <heriroque.22@gmail.com>
2024-12-10 12:10:38 -0500

    feat: add AppSidebar component with navigation links and user info

---
+++ client/src/app/(admin)/dashboard/components/app-sidebar.tsx
@@ -0,0 +1,82 @@
+import { Car, Home, TicketCheck, User, UserCog } from "lucide-react";
+
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+// Menu items.
+const items = [
+  {
+    title: "Inicio",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Vehiculos",
+    url: "/dashboard/vehiculos",
+    icon: Car,
+  },
+  {
+    title: "Usuarios",
+    url: "/dashboard/usuarios",
+    icon: User,
+  },
+  {
+    title: "Empleados",
+    url: "/dashboard/empleados",
+    icon: UserCog,
+  },
+  {
+    title: "Reservas",
+    url: "/dashboard/reservas",
+    icon: TicketCheck,
+  },
+];
+
+// User data
+const data = {
+  user: {
+    name: "Heri Roque",
+    email: "heriroque.22@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/72213898?v=4",
+  },
+};
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Aplicacion</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
