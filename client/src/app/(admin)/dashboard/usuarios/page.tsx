aa447ec12b7d9d8a8c622a04974bba26416fa292
Heri Roque <heriroque.22@gmail.com>
2024-12-10 12:11:34 -0500

    feat: add dashboard and usuarios pages with data fetching and loading state

---
+++ client/src/app/(admin)/dashboard/usuarios/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Usuario } from "@/types/Vehiculo.types";
+import { Table } from "./components/Table";
+import { getAllUsuarios } from "@/services/usuario.service";
+
+export default function Page() {
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function getUsuarios() {
+      try {
+        const { data } = await getAllUsuarios();
+        if (!Array.isArray(data)) {
+          console.log("Error bd no datos");
+          setLoading(false);
+          return;
+        }
+        setUsuarios(data);
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        setLoading(false);
+        setUsuarios([]);
+        console.error(error);
+      }
+    }
+
+    getUsuarios();
+  }, []);
+
+  return (
+    <div>
+      Usuarios List
+      {loading ? <span>Cargando...</span> : <Table usuarios={usuarios} />}
+    </div>
+  );
+}
