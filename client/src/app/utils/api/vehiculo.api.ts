fbb5dddbae70e66ba79c20bf3f5f9f5ca0b0f29d
Heri Roque <heriroque.22@gmail.com>
2024-12-10 12:11:04 -0500

    feat: remove unused API and type files for usuarios and vehiculos

--- client/src/app/utils/api/vehiculo.api.ts
+++
@@ -1,8 +0,0 @@
-import { Vehiculo } from "../types";
-import axiosInstance from "./axiosInstance";
-
-export const getAllVehiculos = () => axiosInstance.get("/api/vehiculos");
-export const getVehiculo = (id: string) => axiosInstance.get(`/api/vehiculos/${id}`);
-export const createVehiculo = (vehiculo: Vehiculo) => axiosInstance.post("/api/vehiculos", vehiculo);
-export const updateVehiculo = (id: string, vehiculo: Vehiculo) => axiosInstance.put(`/api/vehiculos/${id}`, vehiculo);
-export const deleteVehiculo = (id: string) => axiosInstance.delete(`/api/vehiculos/${id}`);
