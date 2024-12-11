73ea847c09fe1332354ff9e5d86288b8d8e0f165
Heri Roque <heriroque.22@gmail.com>
2024-12-11 15:21:15 -0500

    refactor: replace redirect with a simple greeting in Home component; update Providers to conditionally render Navbar based on pathname

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -9,41 +9,30 @@ import {
 } from "@tanstack/react-query";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
-
-// function makeQueryClient() {
-//   return new QueryClient({
-//     defaultOptions: {
-//       queries: {
-//         // With SSR, we usually want to set some default staleTime
-//         // above 0 to avoid refetching immediately on the client
-//         staleTime: 60 * 1000,
-//       },
-//     },
-//   });
-// }
-
-// let browserQueryClient: QueryClient | undefined = undefined;
-
-// function getQueryClient() {
-//   if (isServer) {
-//     // Server: always make a new query client
-//     return makeQueryClient();
-//   } else {
-//     // Browser: make a new query client if we don't already have one
-//     // This is very important, so we don't re-make a new client if React
-//     // suspends during the initial render. This may not be needed if we
-//     // have a suspense boundary BELOW the creation of the query client
-//     if (!browserQueryClient) browserQueryClient = makeQueryClient();
-//     return browserQueryClient;
-//   }
-// }
+import { useState } from "react";
+import { Navbar } from "@/components/Navbar";
+import { usePathname } from "next/navigation";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
-  const queryClient = new QueryClient();
+  // const [queryClient] = useState(
+  //   () =>
+  //     new QueryClient({
+  //       defaultOptions: {
+  //         queries: {
+  //           // Tiempo en el que las consultas se consideran frescas
+  //           staleTime: 5 * 60 * 1000, // 5 minutos
+  //           // Tiempo que los datos de las consultas se mantienen en caché
+  //           gcTime: 10 * 60 * 1000, // 10 minutos
+  //           // Otras configuraciones por defecto pueden añadirse aquí
+  //         },
+  //       },
+  //     })
+  // );
+  const [queryClient] = useState(() => new QueryClient());
+
+  const pathname = usePathname();
+
+  const hiddenNavbar = () => pathname.includes("/dashboard");
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -53,7 +42,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         enableSystem
         disableTransitionOnChange
       >
-        {children}
+        {hiddenNavbar() ? null : <Navbar />}
+
+        <main className="container mx-auto">{children}</main>
       </ThemeProvider>
       <Toaster />
     </QueryClientProvider>
