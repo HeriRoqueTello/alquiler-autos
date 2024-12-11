62ea5f352b7cf701c451e1b041c058ac927a94bd
Heri Roque <heriroque.22@gmail.com>
2024-12-11 17:48:10 -0500

    feat: add protected routes to authentication middleware

--- back/src/server.ts
+++ back/src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { connectDB } from './config/db';
 import vehiculoRoutes from './routes/vehiculo.router';
 import usuarioRoutes from './routes/usuario.router';
+import protectedRoutes from './routes/protected.router';
 
 const app = express();
 
@@ -12,6 +13,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use('/api', [vehiculoRoutes]);
-app.use('/auth', [usuarioRoutes]);
+app.use('/auth', [usuarioRoutes, protectedRoutes]);
+
 
 export default app;
